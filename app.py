4119544a80a5ffc27878b5c4da2f2ff75f828389
TrungThanhTran <tranthanhtrung1990@gmail.com>
2023-09-10 16:53:21 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+import json
+import requests
+from requests.exceptions import ConnectTimeout
+
+import time
+def _call_json_api(api_url, json_data):
+        start = time.time()
+        try:
+            response = requests.post(api_url, json=json_data, timeout=10000)
+        except ConnectTimeout:
+            print('Request has timed out')
+
+        print('time request = ', time.time()-start)
+        print(response)
+        return response.json()
+
+json_data = {
+    "file_name":"health_check_api.mp3"
+    }
+
+api_endpoint = st.text_input('api endpoint', 'cap')
+button = st.button("send api")
+if button:
+    if api_endpoint == 'capturia':
+        api_url = f"https://{api_endpoint}.io/api/v1/transcribe/file"
+        with st.spinner("waiting for proccessing..."):
+            response = _call_json_api(api_url, json_data)
+            st.write(response)
+    else:
+        st.warning('please enter api_endpoint')
